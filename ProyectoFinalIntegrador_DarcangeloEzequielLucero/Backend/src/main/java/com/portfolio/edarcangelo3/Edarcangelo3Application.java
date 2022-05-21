bae9f4ad32ae53d38d8bbf80a7685693ab6fba27
edarcangelo3 <ezequieldarcangelo@hotmail.com>
2022-05-21 03:25:03 -0300

    .

---
+++ ProyectoFinalIntegrador_DarcangeloEzequielLucero/Backend/src/main/java/com/portfolio/edarcangelo3/Edarcangelo3Application.java
@@ -0,0 +1,13 @@
+package com.portfolio.edarcangelo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Edarcangelo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Edarcangelo3Application.class, args);
+	}
+
+}
---
+++ ProyectoFinalIntegrador_DarcangeloEzequielLucero/Backend/src/test/java/com/portfolio/edarcangelo3/Edarcangelo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.edarcangelo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Edarcangelo3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
